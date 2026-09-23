e0a0d705c562aed2ada8597c8ed4631bb49ec7de
megmegmegmegemgemgemgemegm <maria.gomes.nascimento@escola.pr.gov.br>
2026-09-23 08:54:21 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+import javax.swing.*;
+public class Main { //teste
+public static void main(String[] args) {
+SwingUtilities.invokeLater(() -> {
+JFrame frame new JFrame("Snake Game");
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+frame.setSize(480,480);
+frame.setVisible(true);
+});
+}
+}
